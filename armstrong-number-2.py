bb1e1f8cca8f4b3d9f4d24d46972ef366c0b3515
Abdeali Bhagat <abdealibhagat787@gmail.com>
2017-08-07 20:11:58 +0530

    adding armstrong-number-2 file

---
+++ armstrong-number-2.py
@@ -0,0 +1,21 @@
+'''
+  This program prints Armstrong number from 0-1000
+  By AbdealiB
+  https://github.com/AbdealiB/Armstrong-Number
+'''
+
+def armstrong(num):
+    temp = num
+    remainder = 0
+    while temp > 0:
+        remainder += (temp % 10) ** 3
+        temp = temp // 10
+    if num == remainder:
+        return True
+    return False
+
+def main(num=1000):
+    nums = [i for i in range(num) if armstrong(i)]
+    print("The Armstrong Numbers are: ",nums)
+main()
+
